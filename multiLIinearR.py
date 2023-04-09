6261f9d07e2d1af400242d059cc9b6ffbb895f60
taruarora <75572067+taruarora@users.noreply.github.com>
2023-04-09 08:40:54 +0530

    Delete multiLIinearR.py

--- multiLIinearR.py
+++
@@ -1,9 +0,0 @@
-#multivariate linear regression
-from sklearn import datasets
-import pandas as pd
-import numpy as np
-from sklearn import linear_model
-
-
-dataset=datasets.load_boston()
-print(dataset.keys())
